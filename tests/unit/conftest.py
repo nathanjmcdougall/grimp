89e10392417cafbf9f1a4e0d8d46bb6e5d40bb19
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-12-19 10:47:12 +1300

    Use context manager for settings in `configure_unit_tests` fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,14 +1,15 @@
 import pytest  # type: ignore
 
-from grimp.application.config import settings
 from grimp.application.graph import ImportGraph
 from grimp.adaptors.modulefinder import ModuleFinder
+from tests.config import override_settings
 
 
 @pytest.fixture(scope="module", autouse=True)
 def configure_unit_tests():
-    settings.configure(
+    with override_settings(
         IMPORT_GRAPH_CLASS=ImportGraph,
         MODULE_FINDER=ModuleFinder(),
         FILE_SYSTEM=None,
-    )
+    ):
+        yield
